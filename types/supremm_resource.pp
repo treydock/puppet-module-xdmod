631ebfa0cf7d91705571a1051d3ddfc2d15bd9d1
Trey Dockendorf <tdockendorf@osc.edu>
2018-05-23 15:29:07 -0400

    Allow supremm_resource hostname_mode to be set and allow defined enabled to affect config.json for supremm

--- types/supremm_resource.pp
+++ types/supremm_resource.pp
@@ -4,6 +4,7 @@ type Xdmod::Supremm_Resource = Struct[{
   Optional[enabled] => Boolean,
   Optional[datasetmap] => Enum['pcp'],
   Optional[hardware] => Struct[{gpfs => String}],
+  Optional[hostname_mode] => Enum['fqdn','hostname'],
   Optional[pcp_log_dir] => Stdlib::Unixpath,
   Optional[script_dir] => Stdlib::Unixpath,
 }]
