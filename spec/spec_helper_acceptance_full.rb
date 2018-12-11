8d3cf828f170ba8429365895fcbf04ca9ff927cb
Trey Dockendorf <tdockendorf@osc.edu>
2018-12-11 12:05:25 -0500

    Fix tests for puppet6

--- spec/spec_helper_acceptance_full.rb
+++ spec/spec_helper_acceptance_full.rb
@@ -8,10 +8,15 @@ Dir["#{dir}/acceptance/shared_examples/**/*.rb"].sort.each {|f| require f}
 run_puppet_install_helper
 install_module_on(hosts)
 install_module_dependencies_on(hosts)
+collection = ENV['BEAKER_PUPPET_COLLECTION'] || 'puppet5'
 
 RSpec.configure do |c|
   # Readable test descriptions
   c.formatter = :documentation
+  if collection == 'puppet6'
+    on hosts, puppet('module', 'install', 'puppetlabs-yumrepo_core', '--version', '">= 1.0.1 < 2.0.0"'), { :acceptable_exit_codes => [0,1] }
+    on hosts, puppet('module', 'install', 'puppetlabs-cron_core', '--version', '">= 1.0.0 < 2.0.0"'), { :acceptable_exit_codes => [0,1] }
+  end
 end
 
 on hosts, 'puppet config set --section main show_diff true'
