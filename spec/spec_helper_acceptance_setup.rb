79b9cb76921b28f5f825c0eb713cf29fcfe4392c
Trey Dockendorf <tdockendorf@osc.edu>
2020-02-25 14:04:41 -0500

    Fix acceptance tests

--- spec/spec_helper_acceptance_setup.rb
+++ spec/spec_helper_acceptance_setup.rb
@@ -12,3 +12,27 @@ RSpec.configure do |c|
     create_remote_file(hosts, '/etc/sysconfig/mongod', "OPTIONS=\"--smallfiles --quiet -f /etc/mongod.conf\"\n")
   end
 end
+
+# Hack to work around issues with recent systemd and docker and running services as non-root
+if fact('os.family') == 'RedHat' && fact('os.release.major').to_i >= 7
+  hiera_yaml = <<-EOS
+---
+version: 5
+defaults:
+  datadir: data
+  data_hash: yaml_data
+hierarchy:
+  - name: "Common"
+    path: "common.yaml"
+EOS
+  common_yaml = <<-EOS
+---
+pcp::pcp_conf_configs:
+  PCP_USER: root
+  PCP_GROUP: root
+EOS
+
+  create_remote_file(hosts, '/etc/puppetlabs/puppet/hiera.yaml', hiera_yaml)
+  on hosts, 'mkdir -p /etc/puppetlabs/puppet/data'
+  create_remote_file(hosts, '/etc/puppetlabs/puppet/data/common.yaml', common_yaml)
+end
