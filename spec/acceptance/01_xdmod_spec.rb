45073a5c37b4da557d1d953ecc4ee051f1cc6c35
Trey Dockendorf <tdockendorf@osc.edu>
2022-03-24 16:38:37 -0400

    Hacks to fix EL8 acceptance tests

--- spec/acceptance/01_xdmod_spec.rb
+++ spec/acceptance/01_xdmod_spec.rb
@@ -5,6 +5,24 @@ describe 'xdmod class:' do
     it 'runs successfully' do
       pp = <<-EOS
       host { 'xdmod.localdomain': ip => '127.0.0.1' }
+      if versioncmp($facts['os']['release']['major'], '8') >= 0 {
+        $gen_certs = [
+          'sscg -q',
+          '--cert-file /etc/pki/tls/certs/localhost.crt',
+          '--cert-key-file /etc/pki/tls/private/localhost.key',
+          '--ca-file /etc/pki/tls/certs/localhost.crt',
+          '--lifetime 365',
+          "--hostname ${facts['networking']['fqdn']}",
+          "--email root@${facts['networking']['fqdn']}",
+        ]
+        exec { 'httpd-ssl-gencerts':
+          command => join($gen_certs, ' '),
+          path    => '/usr/bin:/usr/sbin:/bin/sbin',
+          creates => '/etc/pki/tls/certs/localhost.crt',
+          before  => Apache::Vhost::Custom['xdmod'],
+          require => Class['apache'],
+        }
+      }
       class { 'mysql::server':
         root_password => 'secret',
       }
